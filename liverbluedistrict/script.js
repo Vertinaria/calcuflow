857e4d44142a4fdf7ca4f12705a4c81118d77c84
Vertinaria <soapeachi@gmail.com>
2025-01-27 11:01:50 +0700

    Update script.js

--- liverbluedistrict/script.js
+++ liverbluedistrict/script.js
@@ -1,180 +1,285 @@
 const H_popup = document.getElementById('H_popup')
 const H = document.getElementById('H')
 const close_H = document.getElementById('close_H')
+
+H.addEventListener('click', () => {
+	H_popup.style.display = 'flex';
+});
+close_H.addEventListener('click', () => {
+	H_popup.style.display = 'none';
+});
+
+const next_He = document.getElementById('next_He')
+next_He.addEventListener('click', () => {
+	H_popup.style.display = 'none';
+	He_popup.style.display = 'flex';
+});
+
 const He_popup = document.getElementById('He_popup')
 const He = document.getElementById('He')
 const close_He = document.getElementById('close_He')
+
 const Li_popup = document.getElementById('Li_popup')
 const Li = document.getElementById('Li')
 const close_Li = document.getElementById('close_Li')
+
 const Be_popup = document.getElementById('Be_popup')
 const Be = document.getElementById('Be')
 const close_Be = document.getElementById('close_Be')
+
 const B_popup = document.getElementById('B_popup')
 const B = document.getElementById('B')
 const close_B = document.getElementById('close_B')
+
 const C_popup = document.getElementById('C_popup')
 const C = document.getElementById('C')
 const close_C = document.getElementById('close_C')
+
 const N_popup = document.getElementById('N_popup')
 const N = document.getElementById('N')
 const close_N = document.getElementById('close_N')
+
 const O_popup = document.getElementById('O_popup')
 const O = document.getElementById('O')
 const close_O = document.getElementById('close_O')
+
 const F_popup = document.getElementById('F_popup')
 const F = document.getElementById('F')
 const close_F = document.getElementById('close_F')
+
 const Ne_popup = document.getElementById('Ne_popup')
 const Ne = document.getElementById('Ne')
 const close_Ne = document.getElementById('close_Ne')
+
 const Na_popup = document.getElementById('Na_popup')
 const Na = document.getElementById('Na')
 const close_Na = document.getElementById('close_Na')
+
 const Mg_popup = document.getElementById('Mg_popup')
 const Mg = document.getElementById('Mg')
 const close_Mg = document.getElementById('close_Mg')
+
 const Al_popup = document.getElementById('Al_popup')
 const Al = document.getElementById('Al')
 const close_Al = document.getElementById('close_Al')
+
 const Si_popup = document.getElementById('Si_popup')
 const Si = document.getElementById('Si')
 const close_Si = document.getElementById('close_Si')
+
 const P_popup = document.getElementById('P_popup')
 const P = document.getElementById('P')
 const close_P = document.getElementById('close_P')
+
 const S_popup = document.getElementById('S_popup')
 const S = document.getElementById('S')
 const close_S = document.getElementById('close_S')
+
 const Cl_popup = document.getElementById('Cl_popup')
 const Cl = document.getElementById('Cl')
 const close_Cl = document.getElementById('close_Cl')
+
 const Ar_popup = document.getElementById('Ar_popup')
 const Ar = document.getElementById('Ar')
 const close_Ar = document.getElementById('close_Ar')
+
 const K_popup = document.getElementById('K_popup')
 const K = document.getElementById('K')
 const close_K = document.getElementById('close_K')
+
 const Ca_popup = document.getElementById('Ca_popup')
 const Ca = document.getElementById('Ca')
 const close_Ca = document.getElementById('close_Ca')
+
 const Sc_popup = document.getElementById('Sc_popup')
 const Sc = document.getElementById('Sc')
 const close_Sc = document.getElementById('close_Sc')
+
 const Ti_popup = document.getElementById('Ti_popup')
 const Ti = document.getElementById('Ti')
 const close_Ti = document.getElementById('close_Ti')
+
 const V_popup = document.getElementById('V_popup')
 const V = document.getElementById('V')
 const close_V = document.getElementById('close_V')
+
 const Cr_popup = document.getElementById('Cr_popup')
 const Cr = document.getElementById('Cr')
 const close_Cr = document.getElementById('close_Cr')
+
 const Mn_popup = document.getElementById('Mn_popup')
 const Mn = document.getElementById('Mn')
 const close_Mn = document.getElementById('close_Mn')
+
 const Fe_popup = document.getElementById('Fe_popup')
 const Fe = document.getElementById('Fe')
 const close_Fe = document.getElementById('close_Fe')
+
 const Co_popup = document.getElementById('Co_popup')
 const Co = document.getElementById('Co')
 const close_Co = document.getElementById('close_Co')
+
 const Ni_popup = document.getElementById('Ni_popup')
 const Ni = document.getElementById('Ni')
 const close_Ni = document.getElementById('close_Ni')
+
 const Cu_popup = document.getElementById('Cu_popup')
 const Cu = document.getElementById('Cu')
 const close_Cu = document.getElementById('close_Cu')
+
 const Zn_popup = document.getElementById('Zn_popup')
 const Zn = document.getElementById('Zn')
 const close_Zn = document.getElementById('close_Zn')
+
 const Ga_popup = document.getElementById('Ga_popup')
 const Ga = document.getElementById('Ga')
 const close_Ga = document.getElementById('close_Ga')
+
 const Ge_popup = document.getElementById('Ge_popup')
 const Ge = document.getElementById('Ge')
 const close_Ge = document.getElementById('close_Ge')
+
 const As_popup = document.getElementById('As_popup')
 const As = document.getElementById('As')
 const close_As = document.getElementById('close_As')
+
 const Se_popup = document.getElementById('Se_popup')
 const Se = document.getElementById('Se')
 const close_Se = document.getElementById('close_Se')
+
 const Br_popup = document.getElementById('Br_popup')
 const Br = document.getElementById('Br')
 const close_Br = document.getElementById('close_Br')
+
 const Kr_popup = document.getElementById('Kr_popup')
 const Kr = document.getElementById('Kr')
 const close_Kr = document.getElementById('close_Kr')
+
 const Rb_popup = document.getElementById('Rb_popup')
 const Rb = document.getElementById('Rb')
 const close_Rb = document.getElementById('close_Rb')
+
 const Sr_popup = document.getElementById('Sr_popup')
 const Sr = document.getElementById('Sr')
 const close_Sr = document.getElementById('close_Sr')
+
 const Y_popup = document.getElementById('Y_popup')
 const Y = document.getElementById('Y')
 const close_Y = document.getElementById('close_Y')
+
 const Zr_popup = document.getElementById('Zr_popup')
 const Zr = document.getElementById('Zr')
 const close_Zr = document.getElementById('close_Zr')
+
+const prev_Y = document.getElementById('prev_Y')
+const next_Nb = document.getElementById('next_Nb')
+prev_Y.addEventListener('click', () => {
+	Zr_popup.style.display = 'none';
+	Y_popup.style.display = 'flex';
+});
+next_Nb.addEventListener('click', () => {
+	Zr_popup.style.display = 'none';
+	Nb_popup.style.display = 'flex';
+});
+
 const Nb_popup = document.getElementById('Nb_popup')
 const Nb = document.getElementById('Nb')
 const close_Nb = document.getElementById('close_Nb')
+
+const prev_Zr = document.getElementById('prev_Zr')
+const next_Mo = document.getElementById('next_Mo')
+prev_Zr.addEventListener('click', () => {
+	Nb_popup.style.display = 'none';
+	Zr_popup.style.display = 'flex';
+});
+next_Mo.addEventListener('click', () => {
+	Nb_popup.style.display = 'none';
+	Mo_popup.style.display = 'flex';
+});
+
 const Mo_popup = document.getElementById('Mo_popup')
 const Mo = document.getElementById('Mo')
 const close_Mo = document.getElementById('close_Mo')
+
+const prev_Nb = document.getElementById('prev_Nb')
+const next_Tc = document.getElementById('next_Tc')
+prev_Nb.addEventListener('click', () => {
+	Mo_popup.style.display = 'none';
+	Nb_popup.style.display = 'flex';
+});
+next_Tc.addEventListener('click', () => {
+	Mo_popup.style.display = 'none';
+	Tc_popup.style.display = 'flex';
+});
+
 const Tc_popup = document.getElementById('Tc_popup')
 const Tc = document.getElementById('Tc')
 const close_Tc = document.getElementById('close_Tc')
+
 const Ru_popup = document.getElementById('Ru_popup')
 const Ru = document.getElementById('Ru')
 const close_Ru = document.getElementById('close_Ru')
+
 const Rh_popup = document.getElementById('Rh_popup')
 const Rh = document.getElementById('Rh')
 const close_Rh = document.getElementById('close_Rh')
+
 const Pd_popup = document.getElementById('Pd_popup')
 const Pd = document.getElementById('Pd')
 const close_Pd = document.getElementById('close_Pd')
+
 const Ag_popup = document.getElementById('Ag_popup')
 const Ag = document.getElementById('Ag')
 const close_Ag = document.getElementById('close_Ag')
+
 const Cd_popup = document.getElementById('Cd_popup')
 const Cd = document.getElementById('Cd')
 const close_Cd = document.getElementById('close_Cd')
+
 const In_popup = document.getElementById('In_popup')
 const In = document.getElementById('In')
 const close_In = document.getElementById('close_In')
+
 const Sn_popup = document.getElementById('Sn_popup')
 const Sn = document.getElementById('Sn')
 const close_Sn = document.getElementById('close_Sn')
+
 const Sb_popup = document.getElementById('Sb_popup')
 const Sb = document.getElementById('Sb')
 const close_Sb = document.getElementById('close_Sb')
+
 const Te_popup = document.getElementById('Te_popup')
 const Te = document.getElementById('Te')
 const close_Te = document.getElementById('close_Te')
+
 const I_popup = document.getElementById('I_popup')
 const I = document.getElementById('I')
 const close_I = document.getElementById('close_I')
+
 const Xe_popup = document.getElementById('Xe_popup')
 const Xe = document.getElementById('Xe')
 const close_Xe = document.getElementById('close_Xe')
+
 const Cs_popup = document.getElementById('Cs_popup')
 const Cs = document.getElementById('Cs')
 const close_Cs = document.getElementById('close_Cs')
+
 const Ba_popup = document.getElementById('Ba_popup')
 const Ba = document.getElementById('Ba')
 const close_Ba = document.getElementById('close_Ba')
+
 const La_popup = document.getElementById('La_popup')
 const La = document.getElementById('La')
 const close_La = document.getElementById('close_La')
+
 const Ce_popup = document.getElementById('Ce_popup')
 const Ce = document.getElementById('Ce')
 const close_Ce = document.getElementById('close_Ce')
+
 const Pr_popup = document.getElementById('Pr_popup')
 const Pr = document.getElementById('Pr')
 const close_Pr = document.getElementById('close_Pr')
+
 const Nd_popup = document.getElementById('Nd_popup')
 const Nd = document.getElementById('Nd')
 const close_Nd = document.getElementById('close_Nd')
@@ -195,12 +300,17 @@ const Gd_popup = document.getElementById('Gd_popup')
 const Gd = document.getElementById('Gd')
 const close_Gd = document.getElementById('close_Gd')
 
-H.addEventListener('click', () => {
-	H_popup.style.display = 'flex';
+const Tb_popup = document.getElementById('Tb_popup')
+const Tb = document.getElementById('Tb')
+const close_Tb = document.getElementById('close_Tb')
+
+Tb.addEventListener('click', () => {
+	Tb_popup.style.display = 'flex';
 });
-close_H.addEventListener('click', () => {
-	H_popup.style.display = 'none';
+close_Tb.addEventListener('click', () => {
+	Tb_popup.style.display = 'none';
 });
+
 He.addEventListener('click', () => {
 	He_popup.style.display = 'flex';
 });
@@ -579,12 +689,6 @@ Gd.addEventListener('click', () => {
 });
 close_Gd.addEventListener('click', () => {
 	Gd_popup.style.display = 'none';
-});
-
-const next_He = document.getElementById('next_He')
-next_He.addEventListener('click', () => {
-	H_popup.style.display = 'none';
-	He_popup.style.display = 'flex';
 });
 
 const prev_H = document.getElementById('prev_H')
